8eb34463bb5dd7fe424252fbac8068a63d0cebbb
gogeebab <47663536+gogeebab@users.noreply.github.com>
2019-11-21 15:43:02 +0300

    Update DataStructureAssinment.java

--- DataStructureAssinment.java
+++ DataStructureAssinment.java
@@ -13,6 +13,17 @@ public class DataStructureAssinment {
         System.out.println("plain text: "+sen);
         System.out.println("the encoded sentenace is: "+n1.encoder(sen, shi));        
         System.out.println("the decoded sentance again: "+n1.decoder(n1.encoder(sen, shi), shi));
+          Duplicate<String> d1 = new Duplicate<String>();
+        d1.addFirst("apple");
+        d1.addFirst("bee");
+        System.out.println("\nthe list contains (apple, bee) ");
+        System.out.println("when there is no duplicates in the list: ");
+        System.out.println(d1.checkDuplicate(d1.head()));
+        d1.addFirst("apple");
+        System.out.println("now the list is (apple, bee, apple) ");
+        System.out.println("when there is duplicates in the list: ");
+        System.out.println(d1.checkDuplicate(d1.head()));
+    }
     }
     
 }
